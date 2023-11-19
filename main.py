85567ac1335a90670ce837aa4b2e66a80ed38960
roman lepihov <lepihovrom@yandex.by>
2023-11-19 22:01:53 +0300

    update

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from PyQt6 import QtWidgets
 import sys
 
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.setWindowTitle("Моя программа")
+        self.resize(300, 70)
+
+        label = QtWidgets.QLabel("<center>Дратути</center>")
+        btnQuit = QtWidgets.QPushButton("Закрыть")
+
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(label)
+        vbox.addWidget(btnQuit)
+
+        self.setLayout(vbox)
+
+        btnQuit.clicked.connect(app.quit)
+
 app = QtWidgets.QApplication(sys.argv)
 
-w = QtWidgets.QWidget()
-w.setWindowTitle("Моя программа")
-w.resize(300, 70)
-
-label = QtWidgets.QLabel("<center>Дратути</center>")
-btnQuit = QtWidgets.QPushButton("Закрыть")
-
-vbox = QtWidgets.QVBoxLayout()
-vbox.addWidget(label)
-vbox.addWidget(btnQuit)
-
-w.setLayout(vbox)
-
-btnQuit.clicked.connect(app.quit)
-
+w = MyWidget()
 w.show()
+
 sys.exit(app.exec())
