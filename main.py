e314782e7f75cdbeae4d5cd9fbafbbe35da13d38
roman lepihov <lepihovrom@yandex.by>
2023-11-21 21:36:52 +0300

    update

--- main.py
+++ main.py
@@ -32,11 +32,19 @@ class MyWidget(QtWidgets.QWidget):
         #     def search(self):
         #         print("Надо что-то найти...")
 
-        self.ui =   my_window.Ui_Form()
+        self.ui = my_window.Ui_Form()
         self.ui.setupUi(self)
 
         # connect signals
-        #self.ui.some_button.connect(self.on_button)
+        self.ui.pushButton_2.clicked.connect(self.close)
+        self.ui.pushButton.clicked.connect(self.search)
+        # def click():
+        #     self.search()
+
+    def search(self):
+        query = self.ui.lineEdit.text()
+        if (query):
+            print("Надо найти: " + self.ui.lineEdit.text())
 
 
 app = QtWidgets.QApplication(sys.argv)
