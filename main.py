f4f4f6e5e212198fb0ce43144c5d0b946a226c09
roman lepihov <lepihovrom@yandex.by>
2023-11-22 14:01:55 +0300

    update

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ import sys
 import my_window
 
 
+def parse_mkb_code(key: str) -> (str, str, str):
+    key = key.replace(".", "")
+    liter = key[0]
+    group = key[1:3]
+
+    if len(key) > 3:
+        num = key[3:len(key)]
+    else:
+        num = ""
+
+    return liter, group, num
+
+
+def get_mkb_code(item: QtWidgets.QTreeWidgetItem) -> str:
+    parent = item.parent()
+    code = item.text(0)
+    while parent:
+        code = parent.text(0) + code
+        parent = parent.parent()
+
+    if len(code) > 3:
+        code = code[0:3] + "." + code[3:len(code)]
+
+    return code
+
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
@@ -35,21 +61,67 @@ class MyWidget(QtWidgets.QWidget):
         self.ui = my_window.Ui_Form()
         self.ui.setupUi(self)
 
+        # self.tree = QtWidgets.QTreeWidget()
+
         # connect signals
         self.ui.pushButton_2.clicked.connect(self.close)
         self.ui.pushButton.clicked.connect(self.search)
+
+        self.ui.treeWidget.currentItemChanged.connect(self.show_decription)
         # def click():
         #     self.search()
 
     def search(self):
         query = self.ui.lineEdit.text()
-        if (query):
-            #rint("Надо найти: " + self.ui.lineEdit.text())
+        if query:
+            # rint("Надо найти: " + self.ui.lineEdit.text())
             self.ui.textBrowser.setText("Надо найти: " + self.ui.lineEdit.text())
+
+    def show_decription(self, item):
+        self.ui.textBrowser.setText(get_mkb_code(item) + " " + item.text(1))
+
+    def fill_tree(self):
+        with open('spr_mkb10.csv', "r", encoding="UTF-8") as file:
+            data = file.readlines()
+
+            parents = []
+            for line in data:
+                arr_line = line.replace("\n", "").replace("\ufeff", "").split(";")
+
+                item = QtWidgets.QTreeWidgetItem()
+                item.setText(1, arr_line[1])
+
+                liter, group, num = parse_mkb_code(arr_line[0])
+
+                if len(parents) > 0:
+                    previous_liter, previous_group, _ = parse_mkb_code(get_mkb_code(parents[len(parents) - 1]))
+                else:
+                    previous_liter, previous_group, = None, None
+
+                if previous_liter != liter:
+                    itemTop = QtWidgets.QTreeWidgetItem()
+                    itemTop.setText(0, liter)
+                    self.ui.treeWidget.addTopLevelItem(itemTop)
+                    parents = [itemTop]
+
+                    parents[len(parents) - 1].addChild(item)
+                    item.setText(0, group)
+                    parents.append(item)
+                else:
+                    if group != previous_group:
+                        parents.pop()
+                        parents[len(parents) - 1].addChild(item)
+                        item.setText(0, group)
+                        parents.append(item)
+                    else:
+                        item.setText(0, num)
+                        parents[len(parents) - 1].addChild(item)
+
 
 app = QtWidgets.QApplication(sys.argv)
 
 w = MyWidget()
+w.fill_tree()
 w.show()
 
 sys.exit(app.exec())
